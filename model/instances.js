cc7debadf1632e7aec0a1e461526cce139f0ebfd
Tommy Romano <tomrom95@gmail.com>
2017-01-20 14:18:37 -0500

    finished item/instance schema

---
+++ model/instances.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var InstanceSchema = new mongoose.Schema({
+  serial_number: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  condition: {
+    type: String,
+    enum: ['GOOD', 'NEEDS_REPAIR'],
+    default: 'GOOD'
+  },
+  status: {
+    type: String,
+    enum: ['AVAILABLE', 'IN_USE', 'LOST'],
+    default: 'AVAILABLE'
+  }
+})
+
+module.exports = mongoose.model('Instance', InstanceSchema);
