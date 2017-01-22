3e6a27d753a87d23ad582b71bf2778093ee5dc6c
Tommy Romano <tomrom95@gmail.com>
2017-01-22 12:38:18 -0500

    added request schema, tests

---
+++ test/test_requests.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+var Request = require('../model/requests.js');
+var Item = require('../model/items.js');
+var assert = require('chai').assert
+
+describe('Instance', function() {
+  it('should throw error for invalid object', function(done) {
+    var request = new Request({
+      user_id: '1234',
+      reason: 'dunno',
+      quantity: 2
+    });
+    request.validate(function (err) {
+      assert.isNotNull(err, 'Invalid object');
+      done();
+    });
+  });
+  it('should validate a valid object', function(done) {
+    var requestJSON = {
+      item_id: '53cb6b9b4f4ddef1ad47f943', // fake valid item id
+      user_id: '1234',
+      reason: 'dunno',
+      quantity: 2
+    };
+    var request = new Request(requestJSON);
+    request.validate(function (err) {
+      assert.isNull(err, 'Valid object');
+      done();
+    });
+  });
+});
