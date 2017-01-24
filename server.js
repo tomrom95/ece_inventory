6433b5fa8b26689742cc7b4213a5f7370ca7a3e7
Henry Yuen <henry.yuen95@gmail.com>
2017-01-24 14:06:05 -0500

    Query without tags working

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ var port = process.env.API_PORT || 3001;
 var dbUser = process.env.DB_USER || "admin";
 var dbPassword = process.env.DB_PASSWORD || "ece458duke";
 
-mongoose.connect('mongodb://' + dbUser + ':' + dbPassword + '@localhost/inventory');
+//mongoose.connect('mongodb://' + dbUser + ':' + dbPassword + '@localhost/inventory');
+mongoose.connect('mongodb://' + 'localhost/test/inventory');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -26,13 +28,51 @@ app.use(function(req, res, next) {
 });
 
 router.route('/inventory')
-  .get(function(req, res) {
-    Item.find(function(err, items){
-      if (err)
-        res.send(err);
-      res.json(items);
-    });
+.get(function(req, res) {
+  // required_tags and excluded_tags: CSV separated values
+  // Remove required_tags and excluded_tags first
+  var required_tags = req.query.required_tags;
+  var excluded_tags = req.query.excluded_tags;
+
+  if(required_tags) required_tags = required_tags.split(',');
+  if(excluded_tags) excluded_tags = excluded_tags.split(',');
+
+  console.log(required_tags);
+
+  delete req.query.required_tags;
+  delete req.query.excluded_tags;
+
+  var filteredList = Item.find(req.query, function(err, items){
+    if(err) res.send(err);
+    // Compile another list by checking each item to see if has any required tags or excluded tags
+
+
+
+
+
+
+
+
+    res.json(items);
+  });
+
   })
+    // var excluded_tags = req.param('excluded_tags');
+    //
+    // Item.find(function(err, items){
+    //   if (err)
+    //     res.send(err);
+    //   res.json(items);
+    // });
+
+
+
+    // Item.find(function(err, items){
+    //   if (err)
+    //     res.send(err);
+    //   res.json(items);
+    // });
+
   .post(function(req, res){
     var item = new Item();
     item.name = req.body.name;
@@ -44,27 +84,27 @@ router.route('/inventory')
     item.has_instance_objects = req.body.has_instance_objects;
     item.save(function(err){
       if(err)
-        return res.send(err);
+      return res.send(err);
       res.json(item);
     })
   })
 
-router.get('inventory/:id', function(req, res) {
-  // res.json({
-  //   id: "12345",
-  //   quantity: 1,
-  //   description: "It's an oscilloscope",
-  //   has_instance_objects: true,
-  // 	tag: ["machine", "expensive"],
-  //   name: "oscilloscope",
-  //   model_number: "23451",
-  //   location: "stockroom",
-  // });
-});
-app.use('/', router);
+  router.get('inventory/:id', function(req, res) {
+    // res.json({
+    //   id: "12345",
+    //   quantity: 1,
+    //   description: "It's an oscilloscope",
+    //   has_instance_objects: true,
+    // 	tag: ["machine", "expensive"],
+    //   name: "oscilloscope",
+    //   model_number: "23451",
+    //   location: "stockroom",
+    // });
+  });
+  app.use('/api', router);
 
-app.listen(port, function() {
-  console.log('API running on port ' + port);
-});
+  app.listen(port, function() {
+    console.log('API running on port ' + port);
+  });
 
-module.exports = app;
+  module.exports = app;
