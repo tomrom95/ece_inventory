334010042e74a53747351f29de191a01106eb5d4
Bitnami <bitnami@ubuntu14-generic-template-01.oit.duke.edu>
2017-01-23 23:34:18 -0500

    updated package.json

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserProfile from './components/UserProfile';
+import Inventory from './components/Inventory';
+import { Link } from 'react-router';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>ECE Laboratory</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          login
         </p>
+	<a className="navbar-brand" href="/components/Inventory"">inventory</a>
+	<div className="collapse navbar-toggleable-md" id="navbarResponsive">
+          <ul className="nav navbar-nav">
+            <li className="nav-item">
+              <Link to="/components/Inventory" className="nav-link">Inventory</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ src/config/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Inventory from '../components/Inventory.js';
+import Home from '../components/Home.js';
+import { Route, IndexRoute } from 'react-router';
+
+export default (
+  <Route>
+    <Route path="/" component={Home}></Route>
+    <Route path="/inventory" component={Inventory}></Route>
+    
+  </Route>
+);
