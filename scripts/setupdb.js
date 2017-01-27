510f7e48c688f075f867b829fe440ecdd18b513f
Tommy Romano <tomrom95@gmail.com>
2017-01-26 19:24:22 -0500

    added setup db script

---
+++ scripts/setupdb.js
@@ -0,0 +1,31 @@
+var Db = require('mongodb').Db,
+  Server = require('mongodb').Server;
+var mongoose = require('mongoose');
+let Item = require('../model/items');
+
+var db = new Db('inventory', new Server('localhost', 27017));
+var fakeJSONData = require('../test/api/test_inventory_GETdata');
+
+db.open(function(err, db) {
+  if (err) { return console.log(err); }
+  db.addUser('admin', 'ece458duke', {
+        roles: [
+            "readWrite",
+            "dbAdmin"
+        ]
+    }, function(err, result) {
+        if (err) {
+          if (err.codeName == 'DuplicateKey') {
+            console.log("you already made an admin user");
+          } else {
+            return console.log(err);
+          }
+        }
+        console.log("Added.");
+        mongoose.connect('mongodb://admin:ece458duke@localhost/inventory');
+        Item.insertMany(fakeJSONData).then(function(obj){
+          console.log("inserted items");
+          end();
+        });
+    });
+});
