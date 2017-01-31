9af47c1634f1c66e5dcafaee93a326c2e602c6e8
Michael Daou <daoumichael@gmail.com>
2017-01-31 14:34:23 -0500

    Switching to master.

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import '../App.css';
 import NavBar from './NavBar.js';
+import AccordionTable from '../AccordionTable.js';
 
 var products = [{
       id: 1,
@@ -21,6 +22,7 @@ class Inventory extends React.Component {
   constructor(props){
     super(props);
   }
+  /*
   render() {
     return (
       <div>
@@ -31,6 +33,14 @@ class Inventory extends React.Component {
         </BootstrapTable>
       </div>
     );
+  }
+  */
+  render() {
+    return (
+      <div>
+        <AccordionTable/>
+      </div>
+      );    
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AccordionTable from './AccordionTable';
+import AccordionTable from './AccordionTable.js';
 import './index.css';
 
 
