3ea60696cd9fd37dd8b9d863e4f4e8353ebf01e1
Tommy Romano <tomrom95@gmail.com>
2017-02-03 17:30:10 -0500

    adding patch

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(passport.initialize());
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Accept, Authorization');
   res.setHeader('Cache-Control', 'no-cache');
   if ('OPTIONS' == req.method) {
