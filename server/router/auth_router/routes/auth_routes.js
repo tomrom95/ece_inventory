9bfd8631c9cd2435b4b4df3c02239fac20ee5e1a
Tommy Romano <tomrom95@gmail.com>
2017-02-10 11:57:08 -0500

    created auth route for oauth login

--- server/router/auth_router/routes/auth_routes.js
+++ server/router/auth_router/routes/auth_routes.js
@@ -1,11 +1,69 @@
 'use strict';
 var jwt = require('jsonwebtoken');
+var axios = require('axios');
 var User = require('../../../model/users');
 var helpers = require('../../../auth/auth_helpers');
 var User = require('../../../model/users');
 
-module.exports.login = function(req, res) {
-  User.findOne({ username: req.body.username }, function(error, user) {
+function lookUpNetIDUser(oauthToken, next) {
+  console.log(oauthToken);
+  axios.get(
+    'https://api.colab.duke.edu/identity/v1/',
+    {
+      headers: {
+        'x-api-key': 'ece-inventory',
+        'Authorization': 'Bearer ' + oauthToken
+      }
+    }
+  ).then(function(response) {
+    next(response.data.error, response.data);
+  }).catch(function(error) {
+    next(error);
+  });
+}
+
+function loginWithOAuth(oauthToken, res) {
+  lookUpNetIDUser(oauthToken, function(error, userInfo) {
+    if (error) return res.send({error: error});
+    User.findOne({netid: userInfo.netid}, function(err, user) {
+      if (err) return res.send({error: error});
+      if (user) return res.json({
+        token: helpers.createAuthToken(user),
+        user: {
+          _id: user._id,
+          netid: user.netid,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          is_admin: user.is_admin,
+          role: user.role
+        }
+      });
+      var user = User({
+        netid: userInfo.netid,
+        first_name: userInfo.firstName,
+        last_name: userInfo.lastName,
+        is_local: false,
+      });
+      user.save(function(error, user) {
+        if (error) return res.send({error: error});
+        return res.json({
+          token: helpers.createAuthToken(user),
+          user: {
+            _id: user._id,
+            netid: user.netid,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            is_admin: user.is_admin,
+            role: user.role
+          }
+        });
+      });
+    });
+  });
+}
+
+function loginWithUsername(username, password, res) {
+  User.findOne({ username: username }, function(error, user) {
     if (error != null) {
       res.send({error: err});
       return;
@@ -13,7 +71,7 @@ module.exports.login = function(req, res) {
       res.send({error: 'User does not exist'});
       return;
     } else{
-      helpers.compare(req.body.password, user.password_hash, function(error, matched) {
+      helpers.compare(password, user.password_hash, function(error, matched) {
         if (error != null) {
           res.send(error);
         } else if (!matched) {
@@ -33,4 +91,12 @@ module.exports.login = function(req, res) {
       })
     }
   });
+}
+
+module.exports.login = function(req, res) {
+  if (req.body.token) {
+    loginWithOAuth(req.body.token, res);
+  } else if (req.body.username && req.body.password) {
+    loginWithUsername(req.body.username, req.body.password, res);
+  }
 }
