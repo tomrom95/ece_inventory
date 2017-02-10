fb678ba89948b7a9206df599aac3ae7df578c112
Henry Yuen <henry.yuen95@gmail.com>
2017-02-10 14:36:07 -0500

    Update inventory_routes.js

--- server/router/api_router/routes/inventory_routes.js
+++ server/router/api_router/routes/inventory_routes.js
@@ -50,7 +50,6 @@ module.exports.getAPI = function (req, res) {
     }
     Item.paginate(query, paginateOptions, function(err, obj){
         if(err) return res.send({error: err});
-        console.log(obj); 
         res.json(obj.docs);
       });
   } else {
