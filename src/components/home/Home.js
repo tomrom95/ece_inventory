3dc6b7817b0cfb05ecd35b8e0a3e595351c4eb79
Tommy Romano <tomrom95@gmail.com>
2017-02-10 12:55:25 -0500

    started netid frontend

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -160,6 +160,8 @@ class Home extends Component {
             <h3 className="row">Log In with your NetID</h3>
             <a href={this.createNetIDLoginLink()} className="btn btn-primary" role="button">NetID Login</a>
           </div>
+          <h3 className="row">Log In with your NetID</h3>
+            <a href={this.createNetIDLoginLink().bind(this)} class="btn btn-primary" role="button">NetID Login</a>
           );
   }
 }
