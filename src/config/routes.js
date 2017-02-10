0eb46aa15268916d4e1cfe4cff80dd4d4620c44c
Tommy Romano <tomrom95@gmail.com>
2017-02-10 13:06:29 -0500

    fixing bugs

--- src/config/routes.js
+++ src/config/routes.js
@@ -115,6 +115,32 @@ function checkForOAuth(nextState, replace) {
   }
 }
 
+import querystring from 'querystring';
+import axios from 'axios';
+
+function checkForOAuth(nextState, replace) {
+  console.log("Next State!")
+  if (nextState.location.hash) {
+    console.log("Has hash!")
+    var token = querystring.parse(nextState.location.hash).access_token;
+    axios.post('https://' + location.hostname + ':3001/auth/login', {
+      token: token
+    }).then(function(result) {
+      if (result.error) {
+        console.log(result.error)
+      } else {
+        console.log(result.data);
+        localStorage.setItem('user', JSON.stringify(result.data.user));
+        localStorage.setItem('token', result.data.token);
+      }
+      location.hash = '';
+    }).catch(function(error) {
+      console.log(error);
+      location.hash = '';
+    });
+  }
+}
+
 export default (
   <Route path="/" component={Home} onEnter={checkForOAuth}>
     <Route path="UserProfile" component={UserProfile}></Route>
