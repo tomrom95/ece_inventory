8e0c701ee78662d8c19c8a4d358c26db41628f99
Henry Yuen <henry.yuen95@gmail.com>
2017-02-13 22:04:07 -0500

    Cart Schema

---
+++ server/model/carts.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var CartSchema = new mongoose.Schema({
+  user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true
+  },
+  item: [
+    {
+      id: {
+        type:mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Item'
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  created: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  lastModified:{
+    type: Date,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Cart', CartSchema);
