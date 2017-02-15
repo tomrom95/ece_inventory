5a9f602d743431a22d3a1f276731675fcac5b5e5
Henry Yuen <henry.yuen95@gmail.com>
2017-02-15 14:24:18 -0500

    Get all carts

--- server/router/api_router/routes/cart_routes.js
+++ server/router/api_router/routes/cart_routes.js
@@ -1,3 +1,14 @@
 'use strict';
 var Cart = require("../../../model/cart");
 var QueryBuilder = require('../../../queries/querybuilder');
+var itemFieldsToReturn = 'name model_number location description';
+
+module.exports.getAPI = function (req, res) {
+    // Admin only - get all carts
+      Cart.find()
+          .populate('item', itemFieldsToReturn)
+          .exec(function(err, cart){
+            if(err) return res.send({error: err});
+            res.json(cart);
+          });
+};
