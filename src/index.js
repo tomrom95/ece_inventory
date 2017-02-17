d1e2e278f37f847b5abfa2cfd1762d25a06b5f23
Michael Daou <daoumichael@gmail.com>
2017-02-17 14:22:20 -0500

    Deleted unused imports and comments in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import ShoppingCartItem from './components/inventory/ShoppingCartItem.js';
-import ShoppingCart from './components/inventory/ShoppingCart.js';
 import './index.css';
 import 'react-select/dist/react-select.css';
 
 ReactDOM.render(<App />,
 	document.getElementById('root')
 );
- 
-//<ShoppingCartItem Name={"Oscilloscope"} Model={"Unknown"} Vendor={"Agilent"} Quantity={10} />
