dd7e47babcb4ff39dc2b9247eabc4563930a1659
Tommy Romano <tomrom95@gmail.com>
2017-03-18 12:21:09 -0400

    refactored

--- scripts/backup_db.js
+++ scripts/backup_db.js
@@ -2,45 +2,74 @@ var exec = require('child_process').exec;
 var fs = require('fs');
 var moment = require('moment');
 
-const BACKUP_VM = 'bitnami@colab-sbx-135.oit.duke.edu'
+const BACKUP_VM = 'bitnami@colab-sbx-135.oit.duke.edu';
+const BACKUP_FOLDER = '~/archives/';
+const REMOVE_SCRIPT = '~/remove_old_backups.sh';
 
-var expiry = 7;
-if (moment().weekday() === 1) {
-  expiry = 28;
+function handleError() {
+  console.log(error);
+  process.exit();
 }
-if (moment().date() === 1) {
-  expiry = 365;
-}
 
-var filename = moment().format('YYYY-MM-DD') + '-exp-' + expiry + '-days.archive';
-var mongoDumpCommand = 'mongodump --archive=' + filename;
-
-exec(mongoDumpCommand, function(error, stdout, stderr) {
-  if (error) {
-    console.log(error);
-    process.exit();
+function getExpiryLength() {
+  var expiry = 7;
+  if (moment().weekday() === 1) {
+    expiry = 28;
   }
-  console.log('Archive created');
-  var scpCommand = 'scp -i ~/.ssh/id_rsa ' + filename + ' ' + BACKUP_VM + ':~/archives/';
+  if (moment().date() === 1) {
+    expiry = 365;
+  }
+  return expiry;
+}
+
+function mongoDump(next) {
+  var expiry = getExpiryLength();
+  var filename = moment().format('YYYY-MM-DD') + '-exp-' + expiry + '-days.archive';
+  var mongoDumpCommand = 'mongodump --archive=' + filename;
+
+  exec(mongoDumpCommand, function(error, stdout, stderr) {
+    if (error) return handleError();
+    return next(filename);
+  });
+}
+
+function scp(filename, vm, folder, next) {
+  var scpCommand = 'scp -i ~/.ssh/id_rsa ' + filename + ' ' + vm + ':' + folder;
   exec(scpCommand, function(error, stdout, stderr) {
-    if (error) {
-      console.log(error);
-      process.exit();
-    }
+    if (error) return handleError();
+    return next();
+  });
+}
+
+function executeVMScript(vm, script, next) {
+  var removeOldCommand = 'ssh -i ~/.ssh/id_rsa ' + vm + ' ' + script;
+  exec(removeOldCommand, function(error, stdout, stderr) {
+    if(error) return handleError();
+    return next();
+  });
+}
+
+function removeLocalCopy(filename, next) {
+  fs.unlink(filename, function(error){
+    if(error) return handleError();
+    return next();
+  });
+}
+
+// Create archive with mongodump
+mongoDump((filename) => {
+  console.log('Archive created with expiry of ' + getExpiryLength() + ' days');
+
+  // Copy archive to backup vm
+  scp(filename, BACKUP_VM, BACKUP_FOLDER, () => {
     console.log('Archive ' + filename + ' sent to backup vm');
-    console.log('Copied archive will be removed in ' + expiry + ' days');
-    var removeOldCommand = 'ssh -i ~/.ssh/id_rsa ' + BACKUP_VM + ' ~/remove_old_backups.sh';
-    exec(removeOldCommand, function(error, stdout, stderr) {
-      if(error) {
-        console.log(error);
-        process.exit();
-      }
-      console.log("Old backups deleted on backup vm")
-      fs.unlink(filename, function(error){
-        if(error) {
-          console.log(error);
-          process.exit();
-        }
+
+    // Execute script on vm to remove old archives
+    executeVMScript(BACKUP_VM, REMOVE_SCRIPT, () => {
+      console.log("Old backups deleted on backup vm");
+
+      // remove local copy here
+      removeLocalCopy(filename, () => {
         console.log('Local archive file deleted');
         process.exit();
       });
