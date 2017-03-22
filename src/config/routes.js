2713d0a7f41df96aff1427d7973ab1b5c0206895
Bitnami <bitnami@ubuntu14-generic-template-01.oit.duke.edu>
2017-03-22 15:56:56 -0400

    emails all on one page and relay proper error message

--- src/config/routes.js
+++ src/config/routes.js
@@ -10,7 +10,6 @@ import DocsView from '../components/docs/DocsView.js';
 import Transactions from '../components/transactions/Transactions.js';
 import LogPage from '../components/logging/LogPage.js';
 import LoanEmailReminders from '../components/emailing/LoanEmailReminders.js';
-import AllRemindersPage from '../components/emailing/AllRemindersPage.js';
 
 import { Route} from 'react-router';
 
@@ -72,11 +71,6 @@ class LoanEmailRemindersWrapper extends Component {
   }
 }
 
-class AllRemindersPageWrapper extends Component {
-  render() {
-    return(<AllRemindersPage/>);
-  }
-}
 
 export default (
   <Route path="/" component={Home}>
@@ -90,6 +84,5 @@ export default (
     <Route path="Transactions" component={TransactionsWrapper}></Route>
     <Route path="Log" component={LogPageWrapper}></Route>
     <Route path="LoanEmailReminders" component={LoanEmailRemindersWrapper}></Route>
-    <Route path="AllRemindersPage" component={AllRemindersPageWrapper}></Route>
   </Route>
 );
