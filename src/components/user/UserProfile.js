236941bb8ddbe3f9b1963b381f7c9848de8c271f
Michael Daou <daoumichael@gmail.com>
2017-03-24 18:17:45 -0400

    Added loans and request tables to user profile page.

--- src/components/user/UserProfile.js
+++ src/components/user/UserProfile.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../../App.css';
 import axios from 'axios';
+import CurrentOrders from '../requests/CurrentOrders.js';
+import LoanPage from '../loans/LoanPage.js';
 
 class UserProfile extends Component {
 
@@ -18,7 +20,7 @@ class UserProfile extends Component {
       role: props.role,
       email: props.email,
       subscribed: props.subscribed ? props.subscribed : false,
-
+      tabSelected: "requests"
     }
   }
 
@@ -73,8 +75,7 @@ class UserProfile extends Component {
       checked={this.state.subscribed}
       onChange={e=>this.handleChange(e)}
       ref={"subcribe-checkbox"}
-      key={"subcribe-checkbox"}
-    />);
+      key={"subcribe-checkbox"} />);
     if(this.state.role === "STANDARD"){
       return null;
     }
@@ -88,7 +89,7 @@ class UserProfile extends Component {
               Subscribing will cause an email to be sent to your account whenever a user request is filed.
             </h6>
             <h6 className="card-title row">
-              {this.state.email}
+              <strong> {this.state.email} </strong>
             </h6>
             <div className="card-title row">
               <label>
@@ -103,23 +104,74 @@ class UserProfile extends Component {
 
   }
 
+  makeTableView() {
+    var table = null;
+    if (this.state.tabSelected === "requests") {
+      table = (<CurrentOrders
+              showFilterBox={false}
+              showStatusFilterBox={true}
+              hasOtherParams={false}/>);
+    }
+
+    else if (this.state.tabSelected === "loans") {
+      table = (<LoanPage isGlobal={true} showFilterBox={true}/>);
+    }
+
+    return (<div className="userprofile-table"> 
+              {table}
+            </div>);
+
+
+  }
+
+  setActiveTab(tab) {
+    this.setState({
+      tabSelected: tab
+    });
+  }
+
   render() {
-
-
     return(
-      <div>
-        <div className="card user-info center-text">
-          <h4 className="card-header">{this.state.username}</h4>
-          <div className="card-block">
-            <h5 className="card-title row">Privilege Level:</h5>
-            <p className="card-title row"> {this.state.role} </p>
-          </div>
-          <div className="card-block">
-            <h5 className="card-title row">Email:</h5>
-            <p className="card-title row"> {this.state.email} </p>
-          </div>
+      <div className="row">
+        <div className="col-md-12">
+          <ul className="nav nav-links userpage-tabs-container">
+            <li className="nav-item userpage-tab-container">
+              <a className="nav-link userpage-tab" 
+                  href="#"
+                  onClick={e => this.setActiveTab("requests")}>
+                  My Requests
+              </a>
+            </li>
+            <li className="nav-item userpage-tab-container">
+              <a className="nav-link userpage-tab" 
+                  href="#"
+                  onClick={e => this.setActiveTab("loans")}>
+                  My Loans
+              </a>
+            </li>
+          </ul>
+          <div className="row userprofile-page">
+            <div className="col-md-9">
+              {this.makeTableView()}
+            </div>
+            <div className="col-md-3">
+              <div className="card user-info center-text">
+              <h4 className="card-header">{this.state.username}</h4>
+              <div className="card-block">
+                <h5 className="card-title row">Privilege Level:</h5>
+                <p className="card-title row"> {this.state.role} </p>
+              </div>
+              <div className="card-block">
+                <h5 className="card-title row">Email:</h5>
+                <p className="card-title row"> {this.state.email} </p>
+              </div>
+            </div>
+            {this.subscribeToEmails()}
+              </div>
+            </div>
         </div>
-        {this.subscribeToEmails()}
+
+
       </div>
     );
   }
