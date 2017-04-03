d07ea1543554b121009612b237fb980effd4c7b8
Henry Yuen <henry.yuen95@gmail.com>
2017-04-03 15:31:40 -0400

    Added contract changes

--- server/router/api_router/routes/minstock_routes.js
+++ server/router/api_router/routes/minstock_routes.js
@@ -5,35 +5,20 @@ var Logger = require('../../../logging/logger');
 var mongoose = require("mongoose");
 
 module.exports.postAPI = function (req, res) {
-  var minStockPromises = [];
-  var itemIDs = [];
-  for(var i = 0; i < req.body.items.length; i++){
-    minStockPromises.push(createMinStockPromise(req.body.items[i].item, req.body.items[i].threshold, req.body.items[i].isEnabled));
-    itemIDs.push(mongoose.Types.ObjectId(req.body.items[i].item));
-  }
-  Promise.all(minStockPromises).then(function(){
-    // return list of updated items
-   Item.find({
-     '_id': {$in: itemIDs}
-   }, function(err, items){
+  if(!req.body.threshold) return res.send({error: "Threshold value needed"});
+  if(!req.body.isEnabled) return res.send({error: "IsEnabled value needed"});
+  Item.update(
+     {_id: {$in: req.body.items}},
+     {$set: {"minstock.threshold": req.body.threshold,
+             "minstock.isEnabled": req.body.isEnabled}},
+     {multi: true},
+   function(err){
      if(err) return res.send({error: err});
-     res.json(items);
-  });
-  }, function(err){
-    return res.send({error: err});
-  });
-}
-
-function createMinStockPromise(item, threshold, isEnabled){
-  return new Promise((resolve, reject) => {
-    Item.findById(item, function(err, itemObj) {
-      if(err) return reject(err);
-      itemObj.minstock.threshold = threshold;
-      itemObj.minstock.isEnabled = isEnabled;
-      itemObj.save(function(err,item){
-        if(err) return reject(err);
-        resolve();
-      })
+     Item.find({
+       '_id': {$in: req.body.items}
+     }, function(err, items){
+       if(err) return res.send({error: err});
+       res.json(items);
     });
   });
 }
