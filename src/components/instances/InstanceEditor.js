5e2e3cf111af6a0e72eab45e809f9627f091e831
Tommy Romano <tomrom95@gmail.com>
2017-04-10 19:59:25 -0400

    search for instance editor

--- src/components/instances/InstanceEditor.js
+++ src/components/instances/InstanceEditor.js
@@ -9,7 +9,8 @@ class InstanceEditor extends Component {
 		super(props);
 		this.state = {
       	itemID: this.props.itemID,
-        allCustomFields: this.props.allCustomFields
+        allCustomFields: this.props.allCustomFields,
+        searchText: ""
 		};
 	}
 
@@ -17,7 +18,8 @@ class InstanceEditor extends Component {
   componentWillReceiveProps(newProps) {
     this.setState({
       itemID: newProps.itemID,
-      allCustomFields: newProps.allCustomFields
+      allCustomFields: newProps.allCustomFields,
+      searchText: ""
     });
   }
 
@@ -26,26 +28,56 @@ class InstanceEditor extends Component {
   }
 
   makeURL() {
-    return 'api/inventory/' + this.state.itemID + '/instances?in_stock=true';
+    var url = 'api/inventory/' + this.state.itemID + '/instances?in_stock=true';
+    if (this.state.searchText) {
+      url += "&tag=" + searchText;
+    }
+    return url;
+  }
+
+  onSearch() {
+    this.setState({
+      searchText: this.refs.tagSearchField.value
+    });
+  }
+
+  renderSearchBar() {
+    return (
+      <div className="row">
+        <div className="col-sm-8">
+          <input
+            type="text"
+            className="form-control form-control-compact"
+            defaultValue={this.state.tag}
+            ref="tagSearchField"
+          />
+        </div>
+        <div className="col-sm-4">
+          <button onClick={() => this.onSearch()} type="button" className="btn btn-sm btn-primary">
+  					<span className="fa fa-search"></span>
+  				</button>
+        </div>
+      </div>
+    )
   }
 
   render() {
     var table = InstanceEditorTable;
     return (
-      <PaginationContainer
-        url={this.makeURL()}
-        processData={data => this.processData(data)}
-        renderComponent={table}
-        showFilterBox={false}
-        showStatusFilterBox={false}
-        hasOtherParams={true}
-        id={'instance-editor-' + this.state.itemID}
-        rowsPerPage={5}
-        extraProps={{
-          allCustomFields: this.state.allCustomFields,
-          itemID: this.state.itemID
-        }}
-      />
+      <div>
+        {this.renderSearchBar()}
+        <PaginationContainer
+          url={this.makeURL()}
+          processData={data => this.processData(data)}
+          renderComponent={table}
+          showFilterBox={false}
+          showStatusFilterBox={false}
+          hasOtherParams={true}
+          id={'instance-editor-' + this.state.itemID}
+          rowsPerPage={5}
+          extraProps={{allCustomFields: this.state.allCustomFields}}
+        />
+      </div>
     );
   }
 }
