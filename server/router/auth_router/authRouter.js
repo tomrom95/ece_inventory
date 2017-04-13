f443725865604247382984e17e6d4ac1dc3c0335
Tommy Romano <tomrom95@gmail.com>
2017-04-13 16:07:42 -0400

    error handling improvements

--- server/router/auth_router/authRouter.js
+++ server/router/auth_router/authRouter.js
@@ -1,7 +1,11 @@
 var auth_routes = require('./routes/auth_routes');
 
 var express = require('express');
+var errorHandler = require('express-async-error').Handler
+
 var router = express.Router();
+
+router.use(errorHandler())
 
 router.route('/login')
       .post(auth_routes.login);
