f2d27207f20f68b6f900a84f8ab35bcffcb8b1d0
Henry Yuen <henry.yuen95@gmail.com>
2017-04-14 04:55:07 -0400

    Single item working, haven't tested out the rest

--- server/router/api_router/routes/inventory_import_routes.js
+++ server/router/api_router/routes/inventory_import_routes.js
@@ -1,6 +1,7 @@
 'use strict';
 var Request = require('../../../model/requests');
 var Item = require('../../../model/items');
+var Instance = require('../../../model/instances');
 var User = require('../../../model/users');
 var CustomField = require('../../../model/customFields.js');
 var CustomFieldHelpers = require('../../../customfields/custom_field_helpers');
@@ -29,7 +30,7 @@ module.exports.postAPI = function (req, res) {
     }
 };
 
-var autoCreateInstances = function(quantity, itemID, importId, next, importinStock=true) {
+var autoCreateInstances = function(quantity, itemID, importId, next, inStock=true) {
   var instances = new Array(quantity);
   for (var i = 0; i < quantity; i++) {
     var newInstance = new Instance({item: itemID, in_stock: inStock});
@@ -39,52 +40,58 @@ var autoCreateInstances = function(quantity, itemID, importId, next, importinSto
 }
 
 var importSingleItem = function(data, next) {
+  if(data.quantity && data.instances && data.instances.length !== 0) return next("Cannot specify both quantity and instances at the same time", null);
   CustomField.find({}).then(function(fieldArray){
    if(data.custom_fields){
      var result = updateCustomFields(data.custom_fields, fieldArray, false, next);
      if(result.error) return next(result.error, null);
      data.custom_fields = result;
    }
-   // Check if the instances array is same size as the quantity, if not autogenerate all instances
-   // Otherwise, call importInstances
-   if(data.instances && data.instances.length === data.quantity){
-     // Call importInstances
-   } else {
-     autoCreateInstances(data.quantity, data.)
-     // auto generate
-
-   }
-
-   // TODO: Remove the instances array field
+   let importId = mongoose.Types.ObjectId();
+   data.import_id = importId;
+   // Set quantity if quantity field is not specified
+   let instances = data.instances;
+   let quantityNotSpecified = data.quantity === undefined || data.quantity === null;
+   if(quantityNotSpecified) data.quantity = instances.length;
+   delete data.instances;
    var item = new Item(data);
    item.save(function(err,item){
      if(err) return next(err,null);
-     return next(null, item);//On successful import
+     // Instances are created here as they require the item to be inserted first
+     if(quantityNotSpecified){
+       importInstances(instances, item._id, importId, function(err, instances){
+         return err ? rollBackAll(importId, err, next) : next(null, item);
+       })
+     } else {
+       autoCreateInstances(data.quantity, item._id, importId, function(err, instances){
+         return err ? rollBackAll(importId, err, next) : next(null, item);
+       })
+     }
    })
  });
 };
 
-var importInstances = function(instancesData, next){
+var importInstances = function(instancesData, itemId, importId, next){
   CustomField.find({}).then(function(fieldArray){
+    console.log(fieldArray);
     var instancesArray = [];
-    let importId = mongoose.Types.ObjectId();
     for(var i in instancesData){
       if(instancesData[i].custom_fields){
-        var result = updateCustomFields(data[i].custom_fields, fieldArray, true, next);
+        var result = updateCustomFields(instancesData[i].custom_fields, fieldArray, true, next);
         if(result.error) return next(result.error, null);
         instancesData[i].custom_fields = result;
       }
       instancesData[i].import_id = importId;
+      instancesData[i].item = itemId;
       instancesArray.push(new Instance(instancesData[i]));
     }
-    Instances.insertMany(instancesArray, function(err, instances){
+    Instance.insertMany(instancesArray, function(err, instances){
+      console.log(err);
+      console.log(instances);
       if(err){
-        // Rollback
-        Instances.remove({import_id: importId}, function(rollBackErr){
-          return rollBackErr ? next(rollBackErr, null) : next(err,null);
-        })
+        rollBackAll(importId, err, next);
       } else {
-        return next(null, instances); // successfully inserted array
+        next(null, instances);
       }
     })
   });
@@ -93,6 +100,8 @@ var importInstances = function(instancesData, next){
 var importMultipleItems = function(data, next){
   CustomField.find({}).then(function(fieldArray){
     var itemArray = [];
+    var quantityNotSpecifiedArray = [];
+    var instancesDataArray = [];
     let importId = mongoose.Types.ObjectId();
     for(var i in data){
       if(data[i].custom_fields){
@@ -101,7 +110,12 @@ var importMultipleItems = function(data, next){
           data[i].custom_fields = result;
       }
       data[i].import_id = importId;
-      // TODO: Remove the instances array field
+      // Set quantity if quantity field is not specified
+      instancesDataArray.push(data[i].instances);
+      delete data[i].instances;
+      let quantityNotSpecified = data.quantity === undefined || data.quantity === null;
+      if(quantityNotSpecified) data.quantity = instances.length;
+      quantityNotSpecifiedArray.push(quantityNotSpecified);
       itemArray.push(new Item(data[i]));
     }
     Item.insertMany(itemArray, function(err, items){
@@ -111,11 +125,38 @@ var importMultipleItems = function(data, next){
           return rollBackErr ? next(rollBackErr, null) : next(err,null);
         })
       } else {
-        return next(null, items); // successfully inserted array
+        // Insert all instances here
+        for(var i in instancesDataArray){
+          // validate custom fields
+          if(instancesDataArray[i].custom_fields){
+            var result = updateCustomFields(instancesDataArray[i].custom_fields, fieldArray, true, next);
+            if(result.error) return next(result.error, null);
+            instancesDataArray[i].custom_fields = result;
+          }
+          if(quantityNotSpecifiedArray[i]){
+            importInstances(instancesDataArray[i], items[i]._id, importId, function(err, instances){
+              if(err) rollBackAll(importId, err, next);
+            })
+          } else {
+            autoCreateInstances(items[i].quantity, items[i]._id, importId, function(err, instances){
+              if(err) rollBackAll(importId, err, next);
+            });
+          }
+        }
+        return next(null, items); // successfully inserted both items and instances
       }
     })
   })
 };
+
+var rollBackAll = function(importId, err, next){
+  Item.remove({import_id: importId}, function(rollBackErr){
+    if(rollBackErr) return next(rollBackErr, null);
+      Instance.remove({import_id: importId}, function(rollBackErr){
+        return rollBackErr ? next(rollBackErr, null) : next(err,null);
+      });
+  })
+}
 
 var updateCustomFields = function(enteredCustomFields, dataCustomFields, perInstance, next){
   var draftCustomFieldArray = [];
