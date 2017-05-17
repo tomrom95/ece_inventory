b5f8d163fc97e6899347c93562cf2199fc6f92f8
Tommy Romano <tomrom95@gmail.com>
2017-05-17 10:50:28 -0400

    Update secrets.js

--- server/secrets.js
+++ server/secrets.js
@@ -2,9 +2,9 @@ module.exports = {
   proxyPort: process.env.PROXY_PORT || 3001,
   productionPort: process.env.PROD_PORT || 443,
   testPort: process.env.TEST_PORT || 3002,
-  dbUser: process.env.DB_USER || "admin",
-  dbPassword: process.env.DB_PASSWORD || "ece458duke",
-  hashSecret: "ece458duke",
-  sslSecret: "ece458duke",
+  dbUser: process.env.DB_USER || "USER",
+  dbPassword: process.env.DB_PASSWORD || "PASSWORD",
+  hashSecret: "HASH",
+  sslSecret: "HASH",
   useProxy: (process.env.USE_PROXY === 'TRUE')
 }
